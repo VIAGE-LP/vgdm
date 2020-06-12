43c2dda69de94620897144101b1b38524d2f539e
Pickachu Chen <pickachu1006@gmail.com>
2020-06-12 15:32:09 +0800

    20200612_line btn edite

--- js/all.js
+++ js/all.js
@@ -14,22 +14,29 @@ $(document).ready(function () {
         var formTop = $('#lp_form').position().top;
         var width = $(window).width();
 
-        if ((formTop <= (scrollPos + windowHeight / 2)) && (width<=768) ) {
+        if ((formTop <= (scrollPos + windowHeight / 2)) && (width <= 768)) {
             $("#fb-root").attr({
-                style:"display: none;"
+                style: "display: none;"
             });
             $("#wh-widget-send-button").attr({
-                style:"display: block;width: 261px;height: 94px;"
+                style: "display: block;width: 261px;height: 94px;"
             });
-        }else if((formTop <= (scrollPos + windowHeight / 2)) && (width >768)){
+        } else if ((formTop <= (scrollPos + windowHeight / 2)) && (width > 768)) {
             $("#wh-widget-send-button").attr({
-                style:"display: none ;width: 0px;height: 0px;"
+                style: "display: none ;width: 0px;height: 0px;"
             });
             $("#fb-root").attr({
-                style:"display: block;"
+                style: "display: block;"
             });
-        }
+        } 
     });
 
+    //偵測sns line btn 手機版還是電腦版，連結不同
+    var vw=$(window).width();
+    if (vw <= 768) {
+        $(".line_link").attr("href", "https://line.me/R/oaMessage/@fhl1857j/?%E7%B4%A2%E5%8F%96%E6%9C%80%E6%96%B0%E5%84%AA%E6%83%A0");
+    } else {
+        $(".line_link").attr("href", "https://line.me/R/ti/p/%40fhl1857j");
+    }
 });
 
