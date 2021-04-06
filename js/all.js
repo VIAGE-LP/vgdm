7a3ea147503d4258bd0a9b72dae8d93f377601e2
Pickachu Chen <pickachu1006@gmail.com>
2021-04-06 15:01:55 +0800

    20210406_add preorder_2

--- js/all.js
+++ js/all.js
@@ -71,12 +71,12 @@ $("#lp_form").ready(function () {
     //購物車下拉選單判斷預購
     $('select[data-name="size-selector"], select[name="color-selector').change(function (e) {
         e.preventDefault();
-        var size_selector = document.querySelector('select[data-name="size-selector"]').value;
-        var color_selector = document.querySelector('select[name="color-selector"]').value;
+        var size_selector = document.querySelector('select[data-category="內衣"][data-name="size-selector"]').value;
+        var color_selector = document.querySelector('select[data-category="內衣"][name="color-selector"]').value;
         var preorder_text = $('<span>', { text: '(預購)', style: 'color: red;' });
         $('.form-group > span').empty();
         if ((color_selector == "天空藍" && size_selector == "M") || (color_selector == "櫻花粉" && size_selector == "M") || (color_selector == "櫻花粉" && size_selector == "M/L") || (color_selector == "海軍藍" && size_selector == "M") || (color_selector == "薰衣草紫" && size_selector == "M") || (color_selector == "寧靜灰" && size_selector == "S") || (color_selector == "異色黑" && size_selector == "S") || (color_selector == "異色黑" && size_selector == "M/L")) {
-            $('select[data-name="size-selector"]').parent().after(preorder_text);
+            $('select[data-category="內衣"][data-name="size-selector"]').parent().after(preorder_text);
         } else {
             $('.form-group > span').empty();
         }
