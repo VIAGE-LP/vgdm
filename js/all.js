7a738d333f13d42526117cf2c9b2200db23541b6
Pickachu Chen <pickachu1006@gmail.com>
2021-05-07 14:58:47 +0800

    20210507_new link vgrpss

--- js/all.js
+++ js/all.js
@@ -7,39 +7,6 @@ $(document).ready(function () {
         }, 900);
     });
 
-    $("#offer_pants img").hide();
-
-    // 點02_btn_bra_SMP會連到並"展開"05_offer_bra_SMP
-    $('a[href="#offer_bra"]').click(function (e) {
-        e.preventDefault();
-        // $("#offer_bra img").attr("style", "display:block;");
-        $('html , body').animate({
-            scrollTop: $('#offer_bra').offset().top,
-        }, 900);
-    });
-    // 點03_btn_pants_SMP會連到並"展開"07_offer_pants_SMP
-    $('a[href="#offer_pants"]').click(function (e) {
-        e.preventDefault();
-        $("#offer_pants img").attr("style", "display:block;");
-        $('html , body').animate({
-            scrollTop: $('#offer_pants').offset().top,
-        }, 900);
-    });
-    // 點06_btn_pants_2_SMP會往上連到03_pants_SMP
-    $('a[href="#pants"]').click(function (e) {
-        e.preventDefault();
-        $('html , body').animate({
-            scrollTop: $('#pants').offset().top,
-        }, 900);
-    });
-    // 點08_btn_set_SMP會往上連到04_offer_set_SMP
-    $('a[href="#offer_set"]').click(function (e) {
-        e.preventDefault();
-        $('html , body').animate({
-            scrollTop: $('#offer_set').offset().top,
-        }, 900);
-    });
-
     // $("#wh-call-to-action").removeAttr("wh-click");
     $("#wh-call-to-action").removeClass("wh-animation-in");
     //聊天機器人
